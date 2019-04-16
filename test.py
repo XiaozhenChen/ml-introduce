5ca0d5cb500244abff9abe35cead7fe359e303ea
unknown <skycxz@qq.com>
2019-04-16 16:03:28 +0800

    “字典数据抽取”

---
+++ test.py
@@ -0,0 +1,19 @@
+from sklearn.feature_extraction import DictVectorizer
+
+def dictvec():
+    """
+    字典数字抽取
+    ：return: None
+    """
+    dict = DictVectorizer()
+
+    data = dict.fit_transform([{'city':'北京'，'temperature':100},
+    {'city':'上海'，'temperature':80},{'city':'深圳'，'temperature':60}])
+
+    print(data)
+
+    return None
+
+
+if __name__=="__main__":
+    dictvec()
